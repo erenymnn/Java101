86af29fb3299d991bbe0d585b4d36d8edb4dcb66
Eren Yaman <erenymnn01@gmail.com>
2025-09-12 00:11:04 +0300

    Hipotenüs hesaplayan program yapıldı.

--- findingTheHypotenuse/src/main/java/org/example/Main.java
+++ findingTheHypotenuse/src/main/java/org/example/Main.java
@@ -1,17 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner tara = new Scanner(System.in);
+
+        System.out.println("Birinci Kenari Giriniz : ");
+        int kenar1 = tara.nextInt();
+        System.out.println("Ikinci Kenari Giriniz : ");
+        int kenar2 = tara.nextInt();
+
+        int hipotenüs=(kenar1*kenar1+kenar2*kenar2);
+
+        System.out.println("Kenarlari = "+kenar1+" a "+kenar1+" olan ucgenlerin dik kenar uzunlugu(Hipotenusu) = "+hipotenüs);
+
+
     }
 }
