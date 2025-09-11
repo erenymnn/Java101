3a47fd1e9dc86c10d4a9981a96a1ff2f54215e7b
Eren Yaman <erenymnn01@gmail.com>
2025-09-11 23:25:41 +0300

    Kdv Tutarı hesaplayan program yapıldı.

---
+++ VatAmountCalculatorProgram/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//     Take the VAT amount as 18%.
+        System.out.println("Lutfen Para degerini giriniz : ");
+
+        double kdvsizFiyat= sc.nextInt();
+        double kdvliFiyat=(((kdvsizFiyat*18)/100)+kdvsizFiyat);
+        double kdvTutari=(kdvliFiyat-kdvsizFiyat);
+
+        System.out.println("KDV'siz fiyat = "+kdvsizFiyat+" KDV'li fiyat = "+kdvliFiyat+" KDVTutari = "+kdvTutari);
+
+
+
+
+
+    }
+}
