f708cefede12100b98d0ec7f6a5c15ea26cbecef
Eren Yaman <erenymnn01@gmail.com>
2025-09-16 18:24:06 +0300

    Fibonacci sequence was made.

--- Fibonacci/src/main/java/org/example/Main.java
+++ Fibonacci/src/main/java/org/example/Main.java
@@ -9,15 +9,15 @@ public class Main {
         System.out.print("Fibonacci dizisi için bir sayı giriniz: ");
         int a = tara.nextInt();
 
-        int n1 = 0, n2 = 1;
+        int k1 = 0, k2 = 1;
 
         System.out.println("Fibonacci Dizisi:");
         for (int i = 0; i < a; i++) {
-            System.out.print(n1 + " "); // mevcut sayıyı yazdır
+            System.out.print(k1 + " "); // mevcut sayıyı yazdır
 
-            int toplam = n1 + n2; // yeni sayı = önceki iki sayı
-            n1 = n2;              // n1 güncelle 2.sayıyı 1. yapıyor
-            n2 = toplam;          // n2 güncelle n2 yi yeni sayı + eski sayıya eşitliyor
+            int toplam = k1 + k2; // yeni sayı = önceki iki sayı
+            k1 = k2;              // n1 güncelle 2.sayıyı 1. yapıyor
+            k2 = toplam;          // n2 güncelle n2 yi yeni sayı + eski sayıya eşitliyor
         }
     }
 }
