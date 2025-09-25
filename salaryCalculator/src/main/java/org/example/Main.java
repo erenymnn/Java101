e0aa856e856d640520042430ab6099b5186ba633
Eren Yaman <erenymnn01@gmail.com>
2025-09-25 18:03:05 +0300

    salary calculation improved.

---
+++ salaryCalculator/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Employee a=new Employee("Eren",2000.0,45,1985);
+        System.out.println(a);
+
+
+
+    }
+}
